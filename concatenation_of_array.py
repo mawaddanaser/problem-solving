715e578c63b067cbd14e09fc5b5111dccd1c8c8b
mawaddanaser <mawaddanaser0@gmail.com>
2024-03-28 01:51:09 +0200

    first problem

---
+++ concatenation_of_array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
+        return nums + nums
+
+    def getConcatenation_2(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        ans_length = 2* len(nums)
+        ans = [0] * ans_length
+        for i in range(0, len(nums)):
+            ans[i] = nums[i]
+            ans[i + n] = nums[i]
+        return ans
